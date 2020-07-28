058de4b24c58406c12bf58f352a7df76fdb27100
dof-bull <donalofarrell1@gmail.com>
2020-07-28 15:58:32 +0000

    Updated Heroku settings

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -23,8 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get(
     'SECRET_KEY',
-    'd+&q1m@o1dm2bjis0r2nh6c8upt9zlt!7jtlf0qxzh(19+&puf'
-    )
+    'd+&q1m@o1dm2bjis0r2nh6c8upt9zlt!7jtlf0qxzh(19+&puf')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
